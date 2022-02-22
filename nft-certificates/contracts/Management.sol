c14ce1050e24758dddfcb18b2c81490bdd13dc81
Mahima Manik <mahimamanik.22@gmail.com>
2022-02-22 17:29:54 +0530

    Adding nftinfo and holdersInfo

--- nft-certificates/contracts/Management.sol
+++ nft-certificates/contracts/Management.sol
@@ -5,12 +5,23 @@ import "./Certificate.sol";
 
 contract Management {
     struct NftInfo {
-        mapping (address => uint) count;
-        address[] addresses;
+        mapping (address => NftHoldersInfo) nftHolders;
+        address[] contractAddresses;
         address owner;
     }
 
+    struct NftHoldersInfo {
+        mapping (address => bool) holdersInfo;
+        address[] holders;
+        address nftAddress;
+    }
+
+    struct Holder {
+        string name;
+    }
+
     event NftCreated(address tokenAddress);
+    event NftIssued(address receiver, address tokenAddress, uint tokenId);
 
     mapping (address => NftInfo) nftadmins;
 
@@ -20,24 +31,35 @@ contract Management {
         address certAddress = cert.contractAddress();
         
         NftInfo storage nftInfo = nftadmins[msg.sender];
-        nftInfo.count[certAddress] = 0;
-        nftInfo.addresses.push(certAddress);
+        NftHoldersInfo storage nftHoldersInfo =  nftInfo.nftHolders[certAddress];
+        nftInfo.contractAddresses.push(certAddress);
         nftInfo.owner = msg.sender;
+        nftHoldersInfo.nftAddress = certAddress;
         
         emit NftCreated(certAddress);
     }
 
-    function issue_certificate(address _receiver, address certAddress, string memory tokenURI) external returns (uint) {
+    function issue_certificate(address _receiver, address certAddress, string memory tokenURI) external {
         NftInfo storage nftInfo = nftadmins[msg.sender];
         require (nftInfo.owner == msg.sender, "Do not have authority to issue this token");
+        // TODO: check if certificate Address is present
+
         Certificate certificate = Certificate(certAddress);
         uint tokenId = certificate.mint(tokenURI, _receiver);
-        nftInfo.count[certAddress] += 1;
-        return tokenId;
+        NftHoldersInfo storage nftHoldersInfo = nftInfo.nftHolders[certAddress];
+        nftHoldersInfo.holdersInfo[_receiver] = true;
+        nftHoldersInfo.holders.push(_receiver);
+        emit NftIssued(_receiver, certAddress, tokenId);
     }
 
     function get_nfts_created() external view returns (address[] memory)   {
         NftInfo storage nftInfo = nftadmins[msg.sender];
-        return nftInfo.addresses;
+        return nftInfo.contractAddresses;
     }
+
+    function get_nft_holders(address certAddress) external view returns (address[] memory) {
+        NftHoldersInfo storage nftHoldersInfo = nftadmins[msg.sender].nftHolders[certAddress];
+        return nftHoldersInfo.holders;
+    }
+
 }
--- nft-certificates/identity.js
+++ nft-certificates/identity.js
@@ -16,7 +16,7 @@ module.exports = async function(callback) {
     }
 
     try {
-        await instance.issue_certificate(accounts[1], nftAddress, "https://ipfs.io/ipfs/QmaZmECGK1fUyuVtp6VJHjBBPgcETGnN6fS2AsDcGLsXBV", {from: accounts[2]});
+        await instance.issue_certificate(accounts[1], nftAddress, "https://ipfs.io/ipfs/QmaZmECGK1fUyuVtp6VJHjBBPgcETGnN6fS2AsDcGLsXBV", {from: accounts[0]});
         console.log("Issued a new token for contract address: ", nftAddress);
     } catch (error) {
         console.log(error);
@@ -25,6 +25,13 @@ module.exports = async function(callback) {
     try {
         let nftsList = await instance.get_nfts_created();
         console.log(nftsList);
+    } catch (error) {
+        console.log(error);
+    }
+
+    try {
+        let holdersList = await instance.get_nft_holders(nftAddress);
+        console.log(holdersList);
     } catch (error) {
         console.log(error);
     }
